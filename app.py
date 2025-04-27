26b0d560426b9168b6cc081cc33c4009d55a0237
Antima <antimanakhat1405@gmail.com>
2025-04-27 22:58:28 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import helper
+import pickle
+
+model = pickle.load(open('E:\DQP\model .pkl','rb'))
+
+st.header('Duplicate Question Pairs')
+st.write("Disclaimer-The model has an accuracy of approx 79%.So there will be some instances where it will predict wrong answers")
+
+q1 = st.text_input('Enter question 1')
+q2 = st.text_input('Enter question 2')
+
+if st.button('Find'):
+    query = helper.query_point_creator(q1,q2)
+    result = model.predict(query)[0]
+
+    if result:
+        st.header('Duplicate')
+    else:
+        st.header('Not Duplicate')
